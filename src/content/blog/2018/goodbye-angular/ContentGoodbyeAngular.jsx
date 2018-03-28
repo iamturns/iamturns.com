81fe6802d820538021c966b8acb567151a6db63a
Matt Turnbull <matt@iamturns.com>
2018-03-28 12:41:07 +1100

    :art: :memo: Angular Burnout article polish
    Merges #5

--- src/content/blog/2018/goodbye-angular/ContentGoodbyeAngular.jsx
+++ src/content/blog/2018/goodbye-angular/ContentGoodbyeAngular.jsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 
+import Emoji from '../../../../components/Emoji';
 import ImgBlock from '../../../../components/ImgBlock';
 import LayoutContent from '../../../../components/LayoutContent';
 import Link from '../../../../components/Link';
@@ -15,7 +16,7 @@ export const frontmatter = {
 		'I dreaded returning to programming during a 3 month travel break. It turns out I’m not done with programming; I’m just done with Angular.',
 	slug: '/angular-burnout',
 	dateCreated: '2018-03-23',
-	dateUpdated: '2018-03-23',
+	dateUpdated: '2018-03-28',
 	type: 'article',
 	nerdsOnly: true,
 	cover: './sleeping-dog.jpg',
@@ -64,67 +65,20 @@ export default ContentGoodbyeAngular;
 const Article = (props: ContentProps) => (
 	<div className="wysiwyg">
 		<p>
-			I dreaded returning to programming during a 3-month travel break.
-			After all these years, it seemed my passion had dried up. Time for
-			something new.
+			I’ve been deep diving into{' '}
+			<Link to="https://angular.io/">Angular</Link> (v1 and 2+) for three
+			years now. During a 3-month travel break, I dreaded returning to
+			programming. After all these years, it seemed my passion had dried
+			up. Time for something new.
 		</p>
 		<p>
 			It turns out I’m not done with programming; I’m just done with
 			Angular.
 		</p>
-		<h2>Oh, Angular…</h2>
-		<p className="p text-sm">*long sigh*</p>
-		<p>
-			I’ve been deep diving into Angular (v1 and 2+) for three years now.
-			Overall, a frustrating experience. Coding feels challenging for the{' '}
-			<em>wrong</em> reasons.
-		</p>
-		<p>A list of things that annoy me daily:</p>
-		<ul>
-			<li>
-				Pointless template syntax
-				<ul>
-					<li>
-						NgIf, NgSwitch, NgFor, what a load of shit. JavaScript
-						already has all these features, and I already know how
-						to use JavaScript — let me use JavaScript!
-					</li>
-					<li>
-						Fun fact: adding <code>else</code> to <code>*ngIf</code>{' '}
-						was a “new feature” in Angular v4!
-					</li>
-				</ul>
-			</li>
-			<li>Tweaking code just so AOT compiler can parse it</li>
-			<li>
-				Zone.js sucks
-				<ul>
-					<li>
-						Errors are garbled, and integrating 3rd-party libs is a
-						pain
-					</li>
-				</ul>
-			</li>
-			<li>
-				General over-engineering
-				<ul>
-					<li>
-						DI with factories and providers, services, ngModules,
-						etc.
-					</li>
-					<li>
-						A lot to learn, a lot to code, and a lot to maintain
-					</li>
-					<li>
-						Many projects succeed without this baggage, is it
-						necessary?
-					</li>
-				</ul>
-			</li>
-		</ul>
 
-		<h2>Arguments for Angular</h2>
-		<blockquote className="blockquote text-lg">
+		<h2>There‘s no need for Angular in 2018</h2>
+
+		<blockquote className="blockquote mt-12 text-lg">
 			“I just need to learn Angular, then I can easily jump between any
 			Angular project.”
 		</blockquote>
@@ -146,15 +100,15 @@ const Article = (props: ContentProps) => (
 			services. Could any Angular developer jump straight into this setup?
 			Nope.
 		</p>
-		<blockquote className="blockquote text-lg">
-			“Code written by Angular official maintainers is better quality and
-			more stable than a bunch of random 3rd-party libraries thrown
-			together.”
+
+		<blockquote className="blockquote mt-12 text-lg">
+			“Code endorsed by the Angular team is better quality and more stable
+			than a bunch of random 3rd-party libraries thrown together.”
 		</blockquote>
 		<p>
 			Angular has mostly settled now, but the early days saw huge breaking
-			changes every month or two. It portrayed a lack of vision and focus.
-			This is not stability.
+			changes every month or two. It portrayed a lack of vision and focus,
+			not stability.
 		</p>
 		<p>The official documentation had this gem:</p>
 		<ImgBlock image={props.data.imgDocs}>
@@ -168,29 +122,36 @@ const Article = (props: ContentProps) => (
 		<p>
 			The JavaScript community is energetic, active, and passionate. A
 			group of random developers can deliver code that surpasses the
-			efforts of core maintainers.
+			efforts of the core Angular maintainers.
 		</p>
-		<blockquote className="blockquote text-lg">
+
+		<blockquote className="blockquote mt-12 text-lg">
 			“I like that Angular makes choices for me, I don’t want to pick and
-			choose which libraries to use.”
+			choose.”
 		</blockquote>
 		<p>
 			I get it. You want to style your React project, okay: CSS Modules or
 			Styled Components? Or maybe Glamorous? Oh, Emotion looks nice!
 		</p>
 		<p>
-			Relying on what Angular provides is initially easier, but it might
-			not be the best for your project. TypeScript is forced on you, but
-			it might be overkill for your needs (the non-TS version of Angular
-			is still undocumented all these years later). RxJS observables for
-			HTTP requests is also mandatory, but using Promises might be a
-			better fit.
+			Relying on what Angular provides is initially easier, but you can be
+			locked into poor choices. TypeScript is forced on you, but it might
+			be overkill for your needs{' '}
+			<span className="text-sm">
+				(the non-TS version of Angular is still undocumented all these
+				years later)
+			</span>. RxJS observables for HTTP requests is also mandatory, but
+			using Promises might be a better fit. Factories, providers,
+			services, ngModules, zone.js, AOT compilation, the list goes on.
 		</p>
 		<p>
-			You can easily become locked into poor choices. Embrace the
-			light-weight libraries that do one thing, and do it well. Celebrate
-			that the JavaScript community provides you with so many options you
-			can pick-and-choose based on your specific needs.
+			Many projects succeed without this baggage. Is it necessary? It‘s a
+			lot to learn, a lot to code, and a lot to maintain.
+		</p>
+		<p>
+			Embrace the light-weight libraries that do one thing, and do it
+			well. Celebrate that the JavaScript community provides you with so
+			many options you can pick the best for your needs.
 		</p>
 
 		<h2>React reignites the passion</h2>
@@ -212,14 +173,18 @@ const Article = (props: ContentProps) => (
 			that best fit my needs. Freedom and power.
 		</p>
 		<p>
-			I love tying together tiny stateless components. It barely looks
-			like framework specific code, and is super quick to scaffold. And,
-			it‘s actually fun!
-		</p>
-		<p>
 			Relying on <Link to="https://prettier.io/">Prettier</Link> to style
 			code for me is a relief. Again, fewer barriers, and more focus on
 			getting the job done.
+		</p>
+		<p>
+			I love tying together tiny stateless components. It barely looks
+			like framework specific code, and is super quick to scaffold.
+			Goodbye NgIf, NgSwitch, NgFor and all that nonsense.{' '}
+			<span className="text-sm">
+				(Fun fact: adding <code>else</code> to <code>*ngIf</code> was a
+				“new feature” in Angular v4 <Emoji>😖</Emoji>)
+			</span>
 		</p>
 		<p>
 			I have only scratched the surface of React, but I can’t deny this
