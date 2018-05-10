17866683900185f7a0a2bd99cfac77f4de2626dd
Matt Turnbull <matt@iamturns.com>
2018-05-10 14:36:56 +1000

    :sparkles: New email CTA for article pages
    Merges #22

---
+++ src/components/LayoutContent/HeaderAuthor.jsx
@@ -0,0 +1,23 @@
+// @flow
+
+import * as React from 'react';
+
+import type { Content } from '../../types/Content';
+import Author from '../Author/Author';
+
+type HeaderAuthorProps = {
+	content: Content,
+};
+
+const LayoutContentHeaderAuthor = ({ content }: HeaderAuthorProps) => {
+	if (!content.frontmatter.dateUpdated) {
+		return null;
+	}
+	return (
+		<div className="my-6">
+			<Author date={content.frontmatter.dateUpdated} />
+		</div>
+	);
+};
+
+export default LayoutContentHeaderAuthor;
